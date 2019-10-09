a86fa8a0e776391e1f609be968f50f5886313d59
Joris Sparla <joris.sparla@gmail.com>
2019-10-09 15:35:33 +0200

    first dev commit

--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ const query = `query a{
     summary
     owner
     status
+    lastupdated
+    title
     service_restored_date
     navid
     
@@ -49,12 +51,8 @@ async function start() {
   const ar = [...sev2];
 
   console.log('----------------------------------');
-  // console.log(
-  //   ar
-  //     .map(inc => ({ ...inc, flag: inc.service_restored_date === null }))
-  //     .filter(({ incident, owner, flag }) => flag)
-  // );
-  // // console.log(sev1);
+  const emailText = fs.readFileSync('./MajorImpact.html', 'utf-8');
+
   const nesev1s = ar
     .map(inc => ({ ...inc, flag: inc.service_restored_date === null }))
     .filter(({ incident, owner, flag }) => flag)
@@ -66,35 +64,19 @@ async function start() {
     });
   // console.log(nesev1s);
   nesev1s.map(async inc => {
-    const subject = ` Severity 2 notification for incident ${inc.incident} - ${inc.customername}`;
-    const body = `
+    const { owner, incident, customername, status, lastupdated, title } = inc;
+    const subject = ` Severity 2 notification for incident ${incident} - ${customername}`;
 
-<p><span>Good morning , ${inc.owner.split(' ')[0]}!</span></p>
-<p><span>You are receiving this email because you have a Severity 2 Incident on your name:</span></p>
-<p><span>&nbsp;</span></p>
-<p><span>Incident ${inc.incident} customer ${inc.customername}</span></p>
-<p><span>&nbsp;</span></p>
-<p><span>Please, note Severity 2 incidents are reserved for issues where a critical business process is impaired and there is no workaround. Our commitment to customers is to update Severity 2 incidents daily (customer should do the same) and we are aiming to resolve them within 5 days or less. </span></p>
-<p><span>We would like to ask you to follow the next steps for this incident:</span></p>
-<p><span>&nbsp;</span></p>
-<p><strong><span style="background: yellow;">1<sup>st</sup> Step &ndash; Check Severity level is correct.</span></strong></p>
-<ul style="list-style-type: disc;">
-    <li><strong><em><span>Severity 2 Definition:</span></em></strong></li>
-</ul>
-<p class="paragraph"><span class="normaltextrun"><strong><span style="text-decoration: underline;">Infrastructure</span></strong></span><span class="eop">&nbsp;</span></p>
-<p class="paragraph"><span class="normaltextrun">Non-production system is unavailable.</span><span class="eop">&nbsp;</span></p>
-<p class="paragraph"><span class="normaltextrun">Production system is unavailable for a substantial number of users.</span><span class="eop">&nbsp;</span></p>
-<p class="paragraph"><span class="normaltextrun"><strong><span style="text-decoration: underline;">Application</span></strong></span><span class="eop">&nbsp;</span></p>
-<p class="paragraph"><span class="normaltextrun">Defined critical business process is impaired, causing serious disruption to operations. Major business process in production system is halted, and no acceptable workaround exists.</span><span class="eop">&nbsp;</span></p>
-<p class="paragraph"><span class="eop" style="font-family: Arial, sans-serif;">&nbsp;</span></p>
-<p>*<strong><em>Awaiting Development</em></strong> = Development has been instructed to <strong>work on Severity 3 as per the new definitions</strong> implemented in March. So there is no need to change to Sev.2 when transferring to Development.</p>
-<p><span>&nbsp;</span></p>
-<p><span>&nbsp;</span></p>
-<p><strong><span style="background: yellow;">2<sup>nd</sup> Step &ndash; If it is a true Severity 2 - Is Service Restored? Make sure you complete the Service Restore Date.</span></strong></p>
-<p class="InforNormalBodyText"><span class="normaltextrun"><strong><span style="color: windowtext; font-family: Calibri, sans-serif;">Service Restored Time</span></strong></span><span class="normaltextrun" style="color: windowtext; font-family: Calibri, sans-serif;"> = &nbsp;we have restore the service, this means the customer has a workaround or a solution to the reported issue. </span></p>
-<p><span>&nbsp;</span></p>
-<p><span>In case of questions or when you see you will not be able to fill our commitments, please contact your Manager.</span></p>
-`;
+    const html = emailText
+      .replace('{incident}', incident)
+      .replace('{incident}', incident)
+      .replace('{incident}', incident)
+      .replace('{customer}', customername)
+      .replace('{status}', status)
+      .replace('{owner}', owner.split(' ')[0])
+      .replace('{lastupdated}', lastupdated)
+      .replace('{title}', title);
+    const body = html;
     // console.log(body);
     const result = await request(uri, mutation, {
       address: inc.email,
